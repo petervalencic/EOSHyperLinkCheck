58addc1a86d8f991a7ababa9283911ae051c23b4
Peter Valenčič <peter.valencic@nielsen.com>
2022-07-26 10:55:10 +0200

    minor changes

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # EOSHyperLinkCheck
+
 A simple scrapper that checks for hyperlinks and verify if the link is accessible or not
 
 # Installation
-``npm install``
 
-To run the code 
-``node /src/inex.js``
+`npm install`
+
+To run the code
+`npm run-script run`
--- src/index.js
+++ src/index.js
@@ -1,35 +1,71 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const chalk = require('chalk');
+const axios = require("axios");
+const cheerio = require("cheerio");
+const chalk = require("chalk");
+const readXlsxFile = require("read-excel-file/node");
 
-const extractLinks = $ => [
-    ...new Set(
-        $('.article a')
-        .map((_, a) => $(a).attr('href'))
-        .toArray()
-    ),
+//.flex-auto .article
+const extractLinks = ($) => [
+  ...new Set(
+    $(".flex-auto a")
+      .map((_, a) => $(a).attr("href"))
+      .toArray()
+  ),
 ];
 
-const verifyLinks = $ => {
-    $.forEach(page => {
-        axios
-        .get(page)
-        .then(response => {
-          if (response.status != 200) {
-            console.log('Hyperlink: ' + chalk.red(page) +' was not found');
-          } else {
-            console.log('Hyperlink: ' + chalk.green(page) +' was found');
-          }
-        });
+const verifyLinks = async (links, pageName) => {
+  console.log("--------------------------------------------------");
+  console.log(chalk.blueBright(pageName) + " will be checked");
+  for (const link of links) {
+    let response = await axios.get(link);
+    if (response.status != 200) {
+      console.log(
+        "Hyperlink: " + chalk.red(link) + ", the remote web page was not found"
+      );
+    } else {
+      console.log(
+        "Hyperlink: " + chalk.green(link) + " the remote web page was found"
+      );
+    }
+  }
+};
 
-    });
-}
+const getData = async (URL) => {
+  const res = await axios(URL);
+  return await res.data;
+};
 
-//request the page
-axios.get('https://help.eossupport.io/en/articles/5697538-why-eos').then(({
-    data
-}) => {
-    const $ = cheerio.load(data);
+const readInputData = async (fileName) => {
+  console.log(fileName);
+  const data = await readXlsxFile(fileName);
+  return await data;
+};
+
+const verifyData = async (data) => {
+  //console.log(data);
+  for (const row in data) {
+    const pageName = data[row][0];
+    const url = data[row][1];
+    console.log(url);
+    const document = await getData(url);
+    console.log(document);
+    const $ = await cheerio.load(document);
     const links = extractLinks($);
-    verifyLinks(links);
+    console.log("links " + links);
+    await verifyLinks(links, pageName);
+  }
+};
+
+//main
+(async () => {
+  console.log(chalk.yellow(`Start `));
+
+  //read excel sheet
+  const excelData = await readInputData(
+    "C:/NodeProjects/peter/EOSHyperLinkCheck/data/test.xlsx"
+  );
+  await verifyData(excelData);
+
+  console.log(chalk.yellow(`End`));
+})().catch((error) => {
+  console.error(error);
 });
